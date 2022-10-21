037891eec225f54580a5c21d9e0be66cfc639b15
bujdos19 <55790467+bujdos19@users.noreply.github.com>
2022-10-21 14:25:12 +0200

    začiatok

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
 // Start here ...
+class Tennis{
+
+
+}
+class Player{
+    PlayerName: String;
+    PlayerNationality: String;
+    PlayerRanking: Number;
+    PlayerRankingPoints: Number;
+
+    constructor (PlayerName: String, PlayerNationality: String, PlayerRanking: Number, PlayerRankingPoints)
+    this.PlayerName= PlayerName;
+    this.PlayerNationality= PlayerNationality;
+    this.PlayerRanking= PlayerRanking;
+    this.PlayerRankingPoints= PlayerRankingPoints;
+}
+const NovakDjokovic= new Player('Novak Djokovic', 'Serbia', 1, )
+class
