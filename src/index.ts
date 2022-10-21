13bad5137a7d916525f3d3f05148f7d5d2892e31
quib963 <55764690+quib963@users.noreply.github.com>
2022-10-21 14:37:51 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-// Start here ...
+class Person {
+  private name: string;
+
+  public constructor(name: string) {
+    this.name = name;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+}
+      
+const person = new Person("Jane");
+
+console.log(person.getName()); // person.name isn't accessible from outside the class since it's private
