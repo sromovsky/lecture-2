2fa89d4ee721295a6ab2fd0d05d30d02507b382a
Tomáš Šromovský <tomas.sromovsky@posam.sk>
2022-11-08 18:58:58 +0100

    Oprava zadania 1 - Tomáš Bujdoš

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,53 @@
-class ScoreBoard{private Set:number; private GameScore: number;
+class ScoreBoard{
 
-constructor(Set: number, GameScore: number)
-{
-this.Set= Set;
-this.GameScore= GameScore;
-}
+    // nazvy parametrov sa oproti triedam zacinaju malym pismenom, info do buducna, nestrhavam body :)
+    private Set:number;
+    private GameScore: number;
 
-getSet(): number {
-return this.Set;
-}
+    constructor(Set: number, GameScore: number)
+    {
+        this.Set= Set;
+        this.GameScore= GameScore;
+    }
 
-getGameScore(): number {
-return this.GameScore;
-}
-}
-class Player{private PlayerName: String;private PlayerNationality: String;private PlayerRanking: Number;private PlayerRankingPoints: Number;
+    getSet(): number {
+        return this.Set;
+    }
 
-constructor (PlayerName: String, PlayerNationality: String, PlayerRanking: Number, PlayerRankingPoints: Number)
-{
-this.PlayerName= PlayerName;
-this.PlayerNationality= PlayerNationality;
-this.PlayerRanking= PlayerRanking;
-this.PlayerRankingPoints= PlayerRankingPoints;}
-
-getPlayerName(): String {
-return this.PlayerName;
-}
-getPlayerNationality(): String {
-return this.PlayerNationality;
+    getGameScore(): number {
+        return this.GameScore;
+    }
 }
 
-getPlayerRanking(): Number {
-return this.PlayerRanking;
+class Player{
+    private PlayerName: String;
+    private PlayerNationality: String;
+    private PlayerRanking: Number;
+    private PlayerRankingPoints: Number;
+
+    constructor (PlayerName: String, PlayerNationality: String, PlayerRanking: Number, PlayerRankingPoints: Number)
+    {
+        this.PlayerName= PlayerName;
+        this.PlayerNationality= PlayerNationality;
+        this.PlayerRanking= PlayerRanking;
+        this.PlayerRankingPoints= PlayerRankingPoints;}
+
+    getPlayerName(): String {
+        return this.PlayerName;
+    }
+    getPlayerNationality(): String {
+        return this.PlayerNationality;
+    }
+
+    getPlayerRanking(): Number {
+        return this.PlayerRanking;
+    }
+
+    getPlayerRankingPoints(): Number {
+        return this.PlayerRankingPoints;
+    }
 }
 
-getPlayerRankingPoints(): Number {
-return this.PlayerRankingPoints;
-}
-}
 // vytvorenie hráčov, dáta z https://www.espn.com/tennis/rankings
 const CarlosAlcaraz= new Player('Carlos Alcaraz', 'Spain', 1, 6730);
 const RafaelNadal= new Player('Rafael Nadal', 'Spain', 2, 5810);
@@ -49,13 +59,22 @@ const NovakDjokovic= new Player('Novak Djokovic', 'Serbia', 7, 4320);
 const AndreyRublev= new Player('Andrey Rublev', 'Russia', 8, 3685)
 const TaylorFritz= new Player('Taylor Fritz', 'USA', 9, 3195)
 
-class Winner{private Pla: Player; private Score: String; private Win:String;
+class Winner{
+    private Pla: Player;
+    private Score: string;
+    private Win: string;
 
-constructor(Pla: Player, Score: String){
-this.Pla= Pla;
-this.Score= Score;
-let Winner: this.getRandomPlayer();
-this.Win= Winner;
+    constructor(Pla: Player, Score: string){
+        this.Pla = Pla;
+        this.Score = Score;
 
-}
+        // zabudnuta implementacia getRandomPlayer? alebo len si nevedel?
+        // let Winner: this.getRandomPlayer();
+        this.Win = '';
+
+
+        // cielom bolo len vyber nahodneho hraca z tabulky?
+        // pozor na String a string String je objekt, na primitivny typ pouzijeme len string
+        // rovnako aj number
+    }
 }
