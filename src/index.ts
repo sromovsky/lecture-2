69b82fce8bdaed1c75da18e81969caa53bd6785f
filipozvat <116357463+filipozvat@users.noreply.github.com>
2022-12-21 02:12:57 +0100

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,138 +1,33 @@
-class Person {
-    private name: string;
-    protected age?: number;
+class Football
+{
 
-    protected constructor(name: string, age?: number) {
-        this.name = name;
-        this.age = age;
+    MatchName: string;
+    MatchCountry: string;
+    Player1Name: string;
+    Player1Country: string;
+    Player2Name: string;
+    Player2Country: string;
+
+    constructor(MatchName: string, MatchCountry: string, MatchStyle: string, Player1Name: string, Player1Country: string, Player2Name: string, Player2Country: string) {
+        this.MatchName = MatchName;
+        this.MatchCountry = MatchCountry;
+        this.Player1Name = Player1Name;
+        this.Player1Country = Player1Country;
+        this.Player2Name = Player2Name;
+        this.Player2Country = Player2Country;
+
     }
 
-    getAge(): number | undefined {
-        return this.age;
+    getMatchInfo(){
+        console.log(`Začína sa futbalový zápas turnaja: ${this.MatchName}`);
+        console.log(`Zápas sa koná v krajine: ${this.MatchCountry}`);
+        console.log("----------------------------------------------------------");
+        console.log(`Meno prvého futbalistu je: ${this.Player1Name}`);
+        console.log(`Prvý futbalista pochádza z: ${this.Player1Country}`);
+        console.log("---");
+        console.log(`Meno druhého futbalista je: ${this.Player2Name}`);
+        console.log(`Druhý futbalista pochádza z: ${this.Player2Country}`);
+        console.log("----------------------------------------------------------");
     }
 
-    getName(): string {
-        return this.name;
-    }
-
-    private ageIncrement() {
-        if (this.age) {
-            this.age++;
-        }
-    }
-
-    birthday() {
-        this.ageIncrement();
-    }
 }
-class Student extends Person {
-    private id: string;
-
-    constructor(name: string, id: string, age?: number) {
-        super(name, age);
-        this.id = id;
-    }
-
-    getId(): string {
-        this.age
-        return this.id;
-    }
-}
-class Teacher extends Person {
-
-    constructor(name: string, age?: number) {
-        super(name, age);
-    }
-}
-
-class Address {
-    private city: string;
-    private street: string;
-    private number: number;
-
-    constructor(city: string, street: string, number: number) {
-        this.number = number;
-        this.city = city;
-        this.street = street;
-    }
-
-    getCity(): string {
-        return this.city;
-    }
-
-    getStreet(): string {
-        return this.street;
-    }
-
-    getNumber(): number {
-        return this.number;
-    }
-}
-
-class Building {
-    private name: string;
-    private address: Address;
-    private floors: Floor[];
-
-    constructor(name: string, address: Address, floorsCount: number) {
-        this.name = name;
-        this.address = address;
-
-        this.floors = [];
-        for (let i = 0; i <= floorsCount; i++) {
-            this.floors.push(new Floor(i));
-        }
-    }
-
-    getName(): string {
-        return this.name;
-    }
-
-    getAddress(): string {
-        return `${this.address.getStreet()} ${this.address.getNumber()}, ${this.address.getCity()}`;
-    }
-
-    getFloor(id: number): Floor | undefined {
-        return this.floors[id];
-    }
-
-    getRoom(roomId: string): Room | undefined {
-        const floorId = Number(roomId.slice(0, roomId.indexOf('.')));
-        const floor = this.getFloor(floorId);
-        return floor?.getRoom(roomId);
-    }
-}
-
-class Room {
-    id: string;
-
-
-    constructor(id: string) {
-        this.id = id;
-    }
-}
-
-class Floor {
-    id: number;
-    rooms: Room[];
-
-    constructor(id: number) {
-        this.id = id;
-
-        this.rooms = []
-        for (let i = 0; i <= 10; i++) {
-            this.rooms.push(new Room(`${this.id}.${i}`));
-        }
-    }
-
-    getRoom(id: string): Room | undefined {
-        return this.rooms.find(r => r.id === id);
-    }
-}
-
-const address1 = new Address('Bratislava', 'Bajkalská', 28);
-const building1 = new Building('FBC', address1, 16);
-
-console.log(building1.getRoom('3.1'));
-
-building1.getRoom('3.1');
