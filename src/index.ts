e1d9a4dc642e70a00727432faef3a1d50e33f6ab
iinesg <115215612+iinesg@users.noreply.github.com>
2022-12-21 02:23:39 +0100

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-// Start here ...
+class Rider {
+
+
+    private riderName: string;
+    private riderSurname: string;
+    private riderTeam: string;
+    private riderCountry: string;
+    private riderPlace: number;
+
+
+    constructor(riderName: string, riderSurname: string, riderTeam: string, riderCountry: string, riderPlace: number) {
+        this.riderName = riderName;
+        this.riderSurname = riderSurname;
+        this.riderTeam = riderTeam;
+        this.riderCountry = riderCountry;
+        this.riderPlace = riderPlace;
+    }
+
+
+
+class StageInfo {
+
+
+    private length: number;
+    private profile: string;
+    private start: string ;
+    private end: string;
+
+    constructor(length: number, profile: string, start: string, end: string) {
+        this.length = length;
+        this.profile = profile;
+        this.start = start;
+        this.end = end;
+    }
+
+
+}
+
+function lap() {
+
+}
+
+
+const stage1 = new StageInfo(171, 'flat', 'Dunkerque', 'Calais');
