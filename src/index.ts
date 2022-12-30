c47a8961ee15badb8ddf5d2fad44b57efab2de33
filipozvat <116357463+filipozvat@users.noreply.github.com>
2022-12-30 19:39:55 +0100

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,24 @@ class Runner {
     private id: number;
     private name: string;
     private surname: string;
-    private  state: string;
+    private startingNumber: number;
+    private agTime: number;
+    private  age: number;
 
-    constructor(id: number, name: string, surname: string, state: string) {
+    constructor(id: number, name: string, surname: string, startingNumber: number, agTime: number, age: number) {
 
         this.id = id;
         this.name = name;
         this.surname = surname;
-        this.state = state;
+        this.startingNumber = startingNumber;
+        this.agTime = agTime;
+        this.age = age;
     }
     getid(): number {
         return this.id;
     }
     getname(): string {
         return this.name;
-    }
-    getsurname(): string {
-        return this.surname;
-    }
-    getstate(): string {
-        return this.state;
     }
 }
 class RaceTrack {
@@ -36,13 +34,14 @@ class RaceTrack {
     }
 }
 
-const runner1 = new Runner(1, "Filip", "Ozvat","Serbia");
-const runner2 = new Runner(2, "Ines", "Gedra ", "Austria");
-const runner3 = new Runner(3, "Nikola", "Marijanović", "Croatia");
+const runner1 = new Runner(1, "Filip", "Ozvat", 92, 300, 25);
+const runner2 = new Runner(2, "Ines", "Gedra ", 17, 280, 30);
+const runner3 = new Runner(3, "Nikola", "Marijanovic", 67, 290, 27);
 
+new RaceTrack(1, 1, 300);
+new RaceTrack(2, 2, 280);
+new RaceTrack(3, 3, 290);
 
-
-
-console.log(runner2.getid(), runner2.getname(), " is on second place");
-console.log(runner3.getid(), runner3.getname(), " is on third place");
-console.log(runner1.getid(), runner1.getname(), " is on first place");
+console.log(runner2.getid(), runner2.getname(), " je na druhom mieste");
+console.log(runner3.getid(), runner3.getname(), " je na tretom mieste");
+console.log(runner1.getid(), runner1.getname(), " je na prvom mieste");
